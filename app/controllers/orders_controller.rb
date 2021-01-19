c34b0fd149aecd8718b5bb81b190a8a563d94276
yesi-aracawa <yesi_yamanaka@hotmail.com>
2021-01-19 01:26:57 -0600

    feat: Make a view compsuming get request
    set alias to the route, make a view taking each attribute from get, make partial index view in order to render it then on index app and make a json response to send it to  view and access as a hash on every attribute

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,12 @@
 class OrdersController < ApplicationController
     def index
-        orders = RestClient::Request.new({
+        response = RestClient::Request.new({
             method: :get,
             url: "https://#{ENV['SHOPIFY_SHOP']}.myshopify.com/admin/api/2021-01/orders.json",
             headers: {'Content_type': 'application/x-www-form-urlencoded', 'X-Shopify-Access-Token': "#{ENV['SHOPIFY_API_TOKEN']}"}
-            }).execute do |response, request, result|
-            case response.code
-            when 400
-                [ :error, JSON.parse(response.to_str) ]
-            when 200
-                [ :success, JSON.parse(response.to_str) ]
-            else
-                fail "Invalid response #{response.to_str} received."
-            end
-         end
+            }).execute
+        @orders = JSON.parse(response)
+        #render json: @orders
         
-        render json: orders
     end
   end
