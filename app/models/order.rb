b0b8d450d3b9c836c3c3fad9d530909bf2506c2a
yesi-aracawa <yesi_yamanaka@hotmail.com>
2021-01-20 00:58:55 -0600

    feat: Send attr to Order object
    Defined object attributes from order, sent status, sku and description, compsuming addres attributes from shopify API

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ApplicationRecord
+    attr_accessor :payment
+    attr_accessor :products
+    attr_accessor :origin
+    attr_accessor :sizes
+    attr_accessor :kind
+    attr_accessor :reference
+    attr_accessor :destiny
+    attr_accessor :items
+    attr_accessor :courier
 end
