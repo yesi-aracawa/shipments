b61f7d505d402986e71d38fff4d5212a7fa61d24
yesi-aracawa <yesi_yamanaka@hotmail.com>
2021-01-20 11:47:38 -0600

    feat: Shipments view and method
    made routes for shipments new instance and create method, it's necessary to send values in labels from order object in order to save values

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :shipments
   get '/orders', to: 'orders#index', as: 'orders'
   get '/orders/:id', to: 'orders#show_id', as: 'order'
+  get '/shipments/new', to: 'shipments#new', as: 'newshipment'
+  get '/shipments/create', to: 'shipments#create', as: 'createshipment'
+
 end
