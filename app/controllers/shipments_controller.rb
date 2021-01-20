e421d7acee1a2a01b371f4210a31b9dee2d009e1
yesi-aracawa <yesi_yamanaka@hotmail.com>
2021-01-20 02:06:26 -0600

    feat: Add shipment Object and details
    Made some methods inside shipment controller, also added respective files in order to configure views

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,10 +1,26 @@
 class ShipmentsController < ApplicationController
+
+    def index
+        @shipments = Shipment.all
+    end
+
     def new
+        @shipment = Shipment.new
     end
 
     def create
+        @shipment = Shipment.new(shipment_params)
+ 
+        @shipment.save
+        redirect_to @shipment
+    end
+       
+    private
+    def shipment_params
+        params.require(:shipment).permit(:order_number, :total_price, :subtotal_price, :status, :sku, :deliver_name)
     end
 
     def show
+         #@shipment = Shipment.find(params[:id])
     end
 end
